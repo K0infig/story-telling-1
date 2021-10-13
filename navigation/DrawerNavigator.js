a724e4dc2e1875059d4e4c9c619f1f3360a75008
K0infig <71273189+K0infig@users.noreply.github.com>
2021-10-13 19:17:46 +0530

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Profile from '../screens/Profile';
+import BottomTabNavigator from './TabNavigation';
+
+const Drawer =createDrawerNavigator();
+
+const DrawerNavigator =() =>{
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={BottomTabNavigator} />
+            <Drawer.Screen name="Profile" component={Profile} />
+        </Drawer.Navigator>
+    )
+
+}
+
+export default DrawerNavigator;
